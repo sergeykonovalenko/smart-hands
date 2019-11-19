6f0858988bc0e4327c441794a722ef194248ed7d
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2019-11-19 14:18:03 +0200

    Завершил верстку главной страницы

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SMART HANDS
-Официальный сайт студии командного коучинга «SMART HANDS»
+Официальный сайт студии командного коучинга «SMART HANDS» для бизнеса в индустрии красоты
 [https://www.smarthands.online](https://www.smarthands.online/)
--- src/js/basic-init.js
+++ src/js/basic-init.js
@@ -1,3 +1,38 @@
 $(document).ready(function () {
+    'use strict';
+
+    // WOW
+    let wow = new WOW(
+        {
+            boxClass: 'wow',
+            animateClass: 'animated',
+            mobile: true,
+        }
+    );
+    wow.init();
+
+    // area slider
+    $('.area-slider-box').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        prevArrow: '',
+        nextArrow: '<button class="area-slider-box__arrow area-slider-box__arrow--next pulse-custom" type="button"><span class="visually-hidden">Вперед</span></button>',
+    });
+
+    // get the width of the scrollbar
+    document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
+
+    // smooth page scrolling
+    $('.scrollto a').click(function () {
+        let elementClick = '#'+$(this).attr('href').split('#')[1];
+        let destination = $(elementClick).offset().top;
+        jQuery('html:not(:animated),body:not(:animated)').animate({scrollTop: destination}, 800);
+        return false;
+    });
 
 }); // end ready
