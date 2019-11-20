3820bd70ceff76677c7b66a7d3888bf4886b0e34
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2019-11-20 14:14:19 +0200

    Завершил верстку страницы ***Программы***

--- src/js/basic-init.js
+++ src/js/basic-init.js
@@ -35,4 +35,83 @@ $(document).ready(function () {
         return false;
     });
 
+    ////////////////////////////////////////////////////////////////////////////
+    // FORM PROCESSING
+
+    $('[data-submit]').on('click', function(e) {
+        e.preventDefault();
+        $(this).parent('form').submit();
+    });
+
+    $.validator.addMethod(
+        "regex",
+        function(value, element, regexp) {
+            let re = new RegExp(regexp);
+            return this.optional(element) || re.test(value);
+        },
+        "Пожалуйста, проверьте свои данные"
+    );
+
+    function valEl(el) {
+        let validator = el.validate({
+            rules:{
+                name:{
+                    required:true
+                },
+                email:{
+                    required:true
+                },
+                phone:{
+                    required:true,
+                    regex: '^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$'
+                }
+            },
+            messages:{
+                name:{
+                    required:''
+                },
+                email:{
+                    required:'',
+                    email:''
+                },
+                phone:{
+                    required:'',
+                    regex:''
+                }
+            },
+            submitHandler: function (form) {
+                $('.modal-order').modal('hide');
+                $('.loader').fadeIn();
+
+                $.ajax({
+                    url: templateUrl + '/tpl-sys-request.php',
+                    type: 'POST',
+                    data: new FormData(form),
+                    processData: false,
+                    contentType: false,
+                })
+                    .always(function (response) {
+                        setTimeout(function () {
+                            $('.loader').fadeOut();
+                        },800);
+                        setTimeout(function () {
+                            $('.modal-thanks').modal('show');
+                            $( '.order__field' ).val('');
+                        },1100);
+                    });
+
+                return false;
+            }
+        });
+    }
+
+    $('.js-form').each(function() {
+        valEl( $(this) );
+    });
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    // masked input
+    $('input[type="tel"]').mask('+7 (999) 999-99-99 ');
+
 }); // end ready
