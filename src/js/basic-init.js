324c825195ec5262fb61ce37562ab8a81f084f6c
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2019-11-21 18:41:22 +0200

    Завершил верстку страницы ***Контакты*** и модального окна

--- src/js/basic-init.js
+++ src/js/basic-init.js
@@ -35,6 +35,22 @@ $(document).ready(function () {
         return false;
     });
 
+    // init popup
+    $('.three-questions-form__button').fancybox({
+        src  : '#js-tips-popup',
+        type : 'inline',
+        touch : false,
+        backFocus : false,
+        btnTpl: {
+            smallBtn:
+               `
+               <button type="button" data-fancybox-close="" class="fancybox-button fancybox-close-small" title="Закрыть">
+                   <svg xmlns="http://www.w3.org/2000/svg" version="1" viewBox="0 0 24 24"><path d="M13 12l5-5-1-1-5 5-5-5-1 1 5 5-5 5 1 1 5-5 5 5 1-1z"></path></svg>
+               </button>
+               `
+        }
+    });
+
     ////////////////////////////////////////////////////////////////////////////
     // FORM PROCESSING
 
